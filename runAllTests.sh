223d76b2c98e7d84008cdcf6ce6ffcaebc808c94
Daniel <danielaustinlee@gmail.com>
2018-10-24 13:30:12 -0400

    Create runAllTests.sh

---
+++ runAllTests.sh
@@ -0,0 +1,46 @@
+#Team Correct Script
+#Members: Hannah Posch, Daniel Lee, Alex Thropp, Daniel Baczmaga
+#runAllTests.sh
+
+#!/bin/bash
+
+#Begin Html file
+# > directs output to file directory.html and overwrites it
+#Next two lines from Team TBD
+file=$(readlink -f "$0")
+filepath=$(dirname "$file")
+
+echo "<html>" > testResults.html
+echo "<title>" >> testResults.html
+echo "Test Results" >> testResults.html
+echo "</title>" >> testResults.html
+echo "</head>" >> testResults.html
+
+echo "<body>" >> testResults.html
+echo "Team Correct - Hannah Posch, Daniel Lee, Alex Thropp, Daniel Baczmaga<br />" >> testResults.html
+echo "<br />" >> testResults.html
+echo "Top Level Directory<br />" >> testResults.html
+echo "Folder: ${PWD##*/}<br />" >> testResults.html
+echo "<br />" >> testResults.html
+
+#For loop to run through directory
+for d in `ls $filepath`; do
+	if [ -f $d -a -x $d]
+	then 
+		$d
+	fi
+	echo "$d<br  />" >> testResults.html
+done
+echo "</body>" >> testResults.html
+echo "</html>" >> testResults.html
+
+#File creation complete output message
+echo "Directory.html file created"
+
+#Open file
+xdg-open directory.html
+
+
+
+
+
