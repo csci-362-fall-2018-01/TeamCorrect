a19ba7bfd89adb6660cf023a9f52d02346bde24c
Hannah Posch <poschhn@g.cofc.edu>
2018-11-07 23:57:14 -0500

    ReminderTest and testCase1 added

--- TestAutomation/project/glucosio-android-develop/app/src/test/java/org/glucosio/android/db/ReminderTest.java
+++ TestAutomation/project/glucosio-android-develop/app/src/test/java/org/glucosio/android/db/ReminderTest.java
@@ -23,4 +23,37 @@ public class ReminderTest {
 
         assertThat(reminderTester.getId()).isEqualTo(id);
     }
+
+    @Test
+    public void SetsId_WhenProvided2() {
+        int id = 123456789;
+
+        reminderTester.setId(id);
+
+        assertThat(reminderTester.getId()).isEqualTo(id);
+    }
+    @Test
+    public void SetsMetric_WhenProvided() {
+        String metric = "0mL";
+
+        reminderTester.setMetric(metric);
+
+        assertThat(reminderTester.getMetric()).isEqualTo(metric);
+    }
+    @Test
+    public void SetsMetric_WhenProvided2() {
+        String metric = "9L";
+
+        reminderTester.setMetric(metric);
+
+        assertThat(reminderTester.getMetric()).isEqualTo(metric);
+    }
+    @Test
+    public void SetsActive_WhenProvided() {
+        boolean active = true;
+
+        reminderTester.setActive(active);
+
+        assertThat(reminderTester.isActive()).isEqualTo(active);
+    }
 }
