ee4ca5704fada6459b386750aa3a724a5edbb2cb
Alex Thropp <alexthropp@alexs-mbp-2.wireless.cougars.int>
2018-11-09 15:55:31 -0500

    add 2 test cases

---
+++ TestAutomation/testCasesExecutables/KeytoneReadingTest.java
@@ -0,0 +1,52 @@
+package org.glucosio.android.db;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+import org.glucosio.android.db.Reminder;
+
+
+public class KeytoneReadingTest {
+
+    private static Reading readingTester = new KetoneReading();
+    public void setup(){
+        readingTester = new KetoneReading();
+    }
+
+    public static void SetsId_WhenProvided() {
+        int id = 1234;
+        readingTester.setId(id);
+        System.out.println("Id returned = " + readingTester.getId());
+    }
+
+
+    public static void SetsId_WhenProvided2() {
+        int id = 146789;
+        readingTester.setId(id);
+        System.out.println("Id returned = " + readingTester.getId());
+
+    }
+
+    public static void SetReading_WhenProvided() {
+        double reading = 2.0;
+        readingTester.setReading(reading);
+        System.out.println("Reading returned = " + readingTester.getReading());
+
+    }
+
+    public static void SetReading_WhenProvided2() {
+        double reading = 10.0;
+        readingTester.setReading(reading);
+        System.out.println("Reading returned = " + readingTester.getReading());
+
+
+    }
+
+    public static void main(String[] args) {
+        SetsId_WhenProvided();
+        SetsId_WhenProvided2();
+        SetReading_WhenProvided();
+        SetReading_WhenProvided2();
+
+    }
+}
