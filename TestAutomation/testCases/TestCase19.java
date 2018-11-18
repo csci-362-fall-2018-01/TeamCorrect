8d66f85c4269dc7f1112f4e0353f637e9ca43cfa
throppa <42742571+throppa@users.noreply.github.com>
2018-11-18 15:14:19 -0500

    Add files via upload
    update

---
+++ TestAutomation/testCases/TestCase19.java
@@ -0,0 +1,49 @@
+package org.glucosio.android.tools;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+package org.glucosio.android.TeamCorrectTestExecutables;
+import org.glucosio.android.TeamCorrectTestExecutables.GlucosioConverter;
+
+
+public class TestCase19 {
+
+    private static GlucosioConverter test = new GlucosioConverter();
+    public void setup(){
+        test = new GlucosioConverter();
+    }
+
+    public static void glucoseToMmolLTest() {
+        double value = 60;
+        System.out.println("glucoseToMmolL: " + test.glucoseToMmolL(value));
+    }
+
+    public static void glucoseToA1CTest() {
+        double value = 60;
+        System.out.println("glucoseToA1C: " + test.glucoseToA1C(value));
+    }
+
+    public static void a1cToGlucoseTest() {
+        double value = 6.0;
+        System.out.println("a1cToGlucose: " + test.a1cToGlucose(value));
+    }
+
+    public static void a1cNgspToIfccTest() {
+        double value = 6.0;
+        System.out.println("a1cNgspToIfcc: " + test.a1cNgspToIfcc(value));
+    }
+
+    public static void a1cIfccToNgspTest() {
+        double value = 64;
+        System.out.println("a1cIfccToNgsp: " + test.a1cIfccToNgsp(value));
+    }
+
+
+    public static void main(String[] args) {
+        glucoseToMmolLTest();
+        glucoseToA1CTest();
+        a1cToGlucoseTest();
+        a1cNgspToIfccTest();
+        a1cIfccToNgspTest();
+    }
+}
