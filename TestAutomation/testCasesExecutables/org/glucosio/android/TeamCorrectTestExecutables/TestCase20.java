2902fa7ef47a4db5fb1c863a3d3da00a3cc0f2be
throppa <42742571+throppa@users.noreply.github.com>
2018-11-18 15:24:41 -0500

    Add files via upload
    fixed bug

--- TestAutomation/testCasesExecutables/org/glucosio/android/TeamCorrectTestExecutables/TestCase20.java
+++ TestAutomation/testCasesExecutables/org/glucosio/android/TeamCorrectTestExecutables/TestCase20.java
@@ -20,29 +20,36 @@ public class TestCase20 {
 
         System.out.println("hourToSpinnerTypeTest: " = tempHours);
         if( tempHours = 0){
+            System.out.println("Spinner: 0");
             System.out.println("Before Breakfast");
         }
         else if(tempHours = 1){
+            System.out.println("Spinner: 1");
             System.out.println("After Breakfast");
 
         }
         else if(tempHours = 2){
+            System.out.println("Spinner: 2");
             System.out.println("Before Lunch");
 
         }
         else if(tempHours = 3){
+            System.out.println("Spinner: 3");
             System.out.println("After Lunch");
 
         }
         else if(tempHours = 4){
+            System.out.println("Spinner: 4");
             System.out.println("Before Dinner");
 
         }
         else if(tempHours = 5){
+            System.out.println("Spinner: 5");
             System.out.println("After Dinner");
         }
         else{
             //8 case
+            System.out.println("Spinner: 8");
             System.out.println("Night Time");
         }
     }
