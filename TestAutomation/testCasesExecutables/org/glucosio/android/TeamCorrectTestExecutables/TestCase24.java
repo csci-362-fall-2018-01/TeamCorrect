f4ea78c2333d1e16d99cd4fe3b14c56ec385087c
TheWinrarz <danielaustinlee@gmail.com>
2018-11-18 22:49:28 -0500

    Update TestCase24

--- TestAutomation/testCasesExecutables/org/glucosio/android/TeamCorrectTestExecutables/TestCase24.java
+++ TestAutomation/testCasesExecutables/org/glucosio/android/TeamCorrectTestExecutables/TestCase24.java
@@ -1,16 +1,18 @@
 package org.glucosio.android.TeamCorrectTestExecutables;
+//Test NumberFormatUtils
 
-
+import java.text.NumberFormat;
 public class TestCase24 {
-
+    private static NumberFormat nfuTester;
     public static void setup(){
-        return;
+        nfuTester = NumberFormatUtils.createDefaultNumberFormat();
     }
 
 
     public static void main(String[] args) {
         setup();
-        System.out.println("Test Passed");
+        System.out.println(nfuTester.getMinimumFractionDigits());
+        System.out.println(nfuTester.getMaximumFractionDigits());
         return;
     }
 }
