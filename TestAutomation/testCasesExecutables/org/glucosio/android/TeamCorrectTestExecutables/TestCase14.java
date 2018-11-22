050f3515182271f18d93841359a793d7effdf096
Daniel Baczmaga <baczmagadk@g.cofc.edu>
2018-11-22 00:34:19 -0500

    Create TestCase14

---
+++ TestAutomation/testCasesExecutables/org/glucosio/android/TeamCorrectTestExecutables/TestCase14.java
@@ -0,0 +1,24 @@
+package org.glucosio.android.TeamCorrectTestExecutables;
+
+public class TestCase014 {
+
+    private static PressureReading pressureReadingTester;
+
+
+    public static void setup(){
+        pressureReadingTester = new PressureReading();
+    }
+
+    public static void SetsMaxReading_WhenProvided() {
+        double maxReading = 180.0;
+
+        pressureReadingTester.setMaxReading(maxReading);
+
+        System.out.println("Max Reading returned = " + pressureReadingTester.getMaxReading());
+    }
+
+    public static void main(String[] args) {
+        setup();
+        SetsMaxReading_WhenProvided();
+    }
+}
