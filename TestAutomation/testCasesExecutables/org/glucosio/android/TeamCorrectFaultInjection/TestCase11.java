5d9e7ae7110335943d93205744aa2a6ffc9084df
throppa <42742571+throppa@users.noreply.github.com>
2018-11-26 18:31:03 -0500

    Create TestCase11.java

---
+++ TestAutomation/testCasesExecutables/org/glucosio/android/TeamCorrectFaultInjection/TestCase11.java
@@ -0,0 +1,24 @@
+
+
+public class TestCase11 {
+
+    private static InputFilterMinMax minMaxTester;
+    private static void setup(){
+        minMaxTester = new InputFilterMinMax(4.0,8.0);
+    }
+
+    public static void isInRangeTester(double a,double b, double c) {
+
+        boolean minMaxReturn = minMaxTester.isInRange(a, b, c);
+
+        System.out.println("Range Tester [" + a + "," + b + "," + c + "] = " + minMaxReturn);
+    }
+
+    public static void main(String[] args) {
+        setup();
+        isInRangeTester(12.0, 1.5, 8,2);
+        
+			
+		}
+
+}
